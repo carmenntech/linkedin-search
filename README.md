4277c6facf6ba48111f917f48ae269570fd79caa
carmenpatit <carmentorrecilla6@gmail.com>
2024-08-10 12:16:41 +0200

    conexion a mongodb

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
+Librerias necesarias para el proyecto 
+pymongo
+
+
 
 links de interes: 
 
 https://learn.microsoft.com/en-us/linkedin/talent/easy-apply
 
 https://pypi.org/project/linkedin-api/#description
+
---
+++ conexion_mongo.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+
+# Conectar a MongoDB (puedes especificar el puerto y la IP si es necesario)
+client = MongoClient("mongodb://localhost:27017/")
+
+# Seleccionar la base de datos (si no existe, MongoDB la crea automáticamente)
+db = client["linkedinapi"]
+
+# Seleccionar la colección (si no existe, MongoDB la crea automáticamente)
+collection = db["borrar"]
+
+# Insertar un documento en la colección
+documento = {
+    "nombre": "Juan",
+    "edad": 28,
+    "ciudad": "Madrid"
+}
+
+# Insertar el documento
+resultado = collection.insert_one(documento)
+
+# Imprimir el ID del documento insertado
+print(f"Documento insertado con el ID: {resultado.inserted_id}")
+
+# Buscar un documento en la colección
+resultado_busqueda = collection.find_one({"nombre": "Juan"})
+
+# Imprimir el documento encontrado
+print("Documento encontrado:", resultado_busqueda)
+
+# Actualizar un documento
+collection.update_one({"nombre": "Juan"}, {"$set": {"edad": 29}})
+
+# Eliminar un documento
+#collection.delete_one({"nombre": "Juan"})
+
+# Cerrar la conexión
+client.close()
+
