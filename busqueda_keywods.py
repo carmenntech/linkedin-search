05afbc3fa82c76b910582963e3d6db6bb6490365
carmenpatit <carmentorrecilla6@gmail.com>
2024-08-11 13:24:26 +0200

    subida a mongo añadiendo la descripcion, revisar los duplicados

--- busqueda_keywods.py
+++ busqueda_keywods.py
@@ -1,23 +1,79 @@
 from linkedin_api import Linkedin
 from credenciales import *
 import pandas as pd
+from pymongo import MongoClient
 import json
 
 api = Linkedin(USER, PWD)
 
-contact_job = api.get_job(job_id=3978495749)
+
+keywordslist = ["Data engieneer", "Data Developer", "Cloud", "Ingeniero de datos", "Big Data", "Inteligencia artificial", "Python Developer", "Data Analyst", "Analista de datos"]
 
 
+contact_jobs2 = api.search_jobs( keywords =  "python" , location_name = "Spain") 
+#print(contact_jobs2)
+# Crear el DataFrame
+df = pd.DataFrame(contact_jobs2)
+df["trackingUrn"] = df["trackingUrn"].apply(lambda x: x[-10:])
+df_id = df["trackingUrn"]
+id_list = df_id.values.tolist()
 
-#print(df)
+print(id_list)
 
-# Claves que quieres seleccionar
-keys_to_select = ["jobPostingId", "title", "formattedLocation"]
-
-# Crear un nuevo diccionario con solo las claves seleccionadas
-selected_data = dict(zip(keys_to_select, [contact_job[key] for key in keys_to_select]))
+df = pd.DataFrame(columns=["jobPostingId", "title", "description", "entityUrn","formattedLocation"])
 
 
+for id in id_list:
 
+    jobpost = api.get_job(job_id=id)
 
-print(selected_data)
+    description = jobpost["description"]["text"] 
+    jobPostingId = jobpost["jobPostingId"] 
+    title = jobpost["title"]
+    entityUrn = jobpost["entityUrn"] 
+    formattedLocation = jobpost["formattedLocation"]
+    
+    # Crear una serie para la fila a añadir
+    new_row = pd.Series(
+        {
+            "jobPostingId": jobPostingId,
+            "title": title,
+            "description": description,
+            "entityUrn": entityUrn,
+            "formattedLocation": formattedLocation
+        }
+    )
+
+    # Añadir la serie al DataFrame
+    
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+    # Conectar a MongoDB (puedes especificar el puerto y la IP si es necesario)
+    client = MongoClient("mongodb://localhost:27017/")
+
+    # Seleccionar la base de datos (si no existe, MongoDB la crea automáticamente)
+    db = client["linkedinapi"]
+
+    # Seleccionar la colección (si no existe, MongoDB la crea automáticamente)
+    collection = db["cloud"]
+
+    #df = pd.concat([df, pd.DataFrame([selected_data])], ignore_index=True)
+
+    # Convertir el DataFrame a una lista de diccionarios
+    data_dict = df.to_dict("records")
+
+    # Insertar los datos en la colección
+    collection.insert_many(data_dict)
+
+   
+
+    #print(jobpost)
+    #print("\n================================")
+    """
+        with open("prueba1.json", "w") as dj:
+        json.dump(df, dj)
+    """
+
+    
+    
+print(df)
--- busquedas_basicas.py
+++ busquedas_basicas.py
@@ -1,26 +1,47 @@
 from linkedin_api import Linkedin
 from credenciales import *
 import json
+import pandas as pd
 
 # Authenticate using any Linkedin account credentials
 api = Linkedin(USER, PWD)
 
 # GET a profile
-profile = api.get_profile('billy-g')
+#profile = api.get_profile('billy-g')
 
 # GET a profiles contact info
-contact_info = api.get_profile_contact_info('billy-g')
+#contact_info = api.get_profile_contact_info('billy-g')
 
 # GET 1st degree connections of a given profile
-connections = api.get_profile_connections('1234asc12304')
+#connections = api.get_profile_connections('1234asc12304')
 
-print(contact_info)
+#print(contact_info)
 
 contact_job = api.get_job(job_id=3972627974)
 
-print(contact_job)
+
+#print(contact_job)
+description = contact_job["description"]["text"] 
+jobPostingId = contact_job["jobPostingId"] 
+title = contact_job["title"]
+entityUrn = contact_job["entityUrn"] 
+formattedLocation = contact_job["formattedLocation"] 
+
+df = pd.DataFrame({'jobPostingId': [jobPostingId],  'title': [title], 'description': [description],  'entityUrn': [entityUrn], 'formattedLocation': [formattedLocation]})
 
 
+# Mostrar el DataFrame
+print(df)
+
+#print(text)
+
+##keys_to_select = str(["jobPostingId", "title", "entityUrn"]) + str(["description"]["text"])
+
+# Crear un nuevo diccionario con solo las claves seleccionadas
+#selected_data = dict(zip(keys_to_select, [contact_job[key] for key in keys_to_select]))
+
+
+#print(keys_to_select)
 
 #contact_jobspython = api.search_jobs(selectors=[{'jobs': ['id', 'customer-job-code', 'posting-date']}], params={'title': 'mongodb django', 'count': 2})
 
