25c6f35f8dab7f7155f42518cd1ab07949734b6f
carmenpatit <carmentorrecilla6@gmail.com>
2024-08-20 14:19:56 +0200

    busquedas por keywork + get_porfile_skill

--- busquedas-pruebas.py
+++ busquedas-pruebas.py
@@ -18,20 +18,6 @@ from linkedin_api.utils.helpers import (
 # Authenticate using any Linkedin account credentials
 api = Linkedin(USER, PWD)
 
+resultados = api.get_profile_skills(public_id = 'carlosazaustre')
 
-
-contact_job = api.search_people(keywords = 'Mongodb', schools='Universidad de Valladolid')
-
-
-# Conectar a MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client["linkedinapi"]
-collection = db["perfiles-mogo"]
-
-df = pd.DataFrame(contact_job)
-
-# Insertar los datos en la colección
-collection.insert_many(df.to_dict("records"))
-
-
-print(contact_job)
+print(resultados)
