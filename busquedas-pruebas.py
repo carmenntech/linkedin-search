3ddf2ecc3cf9282142db156a0b181cc8a4dd116e
carmenpatit <carmentorrecilla6@gmail.com>
2024-08-21 13:04:54 +0200

    busqueda skills x perfiles

--- busquedas-pruebas.py
+++ busquedas-pruebas.py
@@ -18,6 +18,39 @@ from linkedin_api.utils.helpers import (
 # Authenticate using any Linkedin account credentials
 api = Linkedin(USER, PWD)
 
-resultados = api.get_profile_skills(public_id = 'carlosazaustre')
+#resultados = api.get_profile_skills(public_id = 'carlosazaustre')
 
-print(resultados)
+# Realizar un find() sin filtro (traerá todos los documentos de la colección)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["linkedinapi"]
+collection = db["perfiles-mongo"]
+urn_perfiles = collection.find({}, {"urn_id": 1, "_id":0})
+print(type(urn_perfiles))
+urn_perfiles = [item['urn_id'] for item in urn_perfiles]
+urn_values = list(urn_perfiles)
+rows = []
+
+# Iterar sobre los resultados e imprimirlos
+for urn in urn_values:
+
+    resultados = api.get_profile_skills(urn_id = urn)
+
+    # Extraer solo los valores del campo 'name'
+    lista_items = [item['name'] for item in resultados]
+
+    # Añadir un nuevo diccionario con los datos a la lista de filas
+    rows.append({
+        "urn": urn,
+        "list_items": lista_items,
+    })
+
+# Convertir la lista de filas en un DataFrame
+df = pd.DataFrame(rows)
+
+# Conectar a MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["linkedinapi"]
+collection = db["items-perfil-mongo"]
+
+# Insertar los datos en la colección
+collection.insert_many(df.to_dict("records"))
